54aec1e5fb049ed6c5b7a119d3dc1d2ff129a47e
CHAN Vonjy Tahina <51434462+vonjytahina@users.noreply.github.com>
2023-07-14 12:51:54 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <HashRouter> 
       <>
         <Routes>
-          <Route path="/" element={<Home />} />       
+          <Route path="" element={<Home />} />       
           <Route path="/destination" element={<Destination />} />       
           <Route path="/crew" element={<Crew />} />       
           <Route path="/technology" element={<Technology/>} />       
